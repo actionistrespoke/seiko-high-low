daf82a4987bc72beaccd2260e112a2de8f26837a
actionistrespoke <127120065+actionistrespoke@users.noreply.github.com>
2023-03-11 23:44:49 +0000

    Add files via upload

---
+++ sprites/lobokoni-cutscenes/introcutscene.lua
@@ -0,0 +1,78 @@
+return {
+	imageSrc = "sprites/cutscene/intro/",
+	imageBorderSrc = "sprites/cutscene/border.png",
+	imageBackgroundSrc = "sprites/cutscene/bg.png",
+	frames = {
+		{
+			duration = 15,
+			sound = "/sfx/tokyo_ambient.ogg",
+			volume = .5,
+			caption = "Seiko was out on the streets, being stupid, as always."
+		},
+		{
+			duration = 10,
+			sound = "/sfx/holy.wav",
+			caption = "Then, thinking of a present for her girlfriend, she noticed something she probably couldn't afford."
+		},
+		{
+			duration = 8,
+			caption = "\"Wow, that sure looks cool!\", she thought, oblivious to the fact that she was in irreparable debt."
+		},
+		{
+			duration = 1,
+			caption = "Regardless, she figured it'd be the perfect present for her girlfriend, who doesn't actually like her."
+		},
+		{
+			duration = .25,
+			caption = "Regardless, she figured it'd be the perfect present for her girlfriend, who doesn't actually like her."
+		},
+		{
+			duration = .25,
+			caption = "Regardless, she figured it'd be the perfect present for her girlfriend, who doesn't actually like her."
+		},
+		{
+			duration = .25,
+			caption = "Regardless, she figured it'd be the perfect present for her girlfriend, who doesn't actually like her."
+		},
+		{
+			duration = 5,
+			sound = "/sfx/success.wav",
+			caption = "Regardless, she figured it'd be the perfect present for her girlfriend, who doesn't actually like her."
+		},
+		{
+			duration = 8,
+			sound = "/sfx/pockets.ogg",
+			caption = "Seiko thoroughly searched her pockets for a way to cover the cost of the plush toy."
+		},
+		{
+			duration = 10,
+			caption = "Still standing in the middle of the street, after conducting an extensive search, she stared vacuously at her palm for a couple of minutes."
+		},
+		{
+			duration = 8,
+			sound = "/sfx/fall.wav",
+			volume = .66,
+			caption = "Unsurprisingly, she wasn't able to find more than a few cents. Everyone nearby stopped what they were doing to point and laugh."
+		},
+		{
+			duration = 7,
+			offsetStart = {x = 0, y = 0},
+			offsetEnd = {x = 0, y = 500},
+			overlayBorder = true,
+			withBackground = true,
+			sound = "/sfx/paper.wav",
+			caption = "Did I mention she carelessly dropped that paper you saw earlier? You know, 'cause she's dumb."
+		},
+		{
+			duration = 10,
+			offsetStart = {x = 0, y = -700},
+			offsetEnd = {x = 0, y = 0},
+			overlayBorder = true,
+			caption = "\"Ah...\""
+		},
+		{
+			duration = 15,
+			caption = {"\"I see you're in need of some cash!\"","\"I could help you out with that, as long as you're willing to play a little game...\""}
+		}
+	}
+}
